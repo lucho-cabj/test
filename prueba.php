4a15c13b7619c45b19d11884db2016c36a7c3998
lucho-cabj <guter_cabj@hotmail.com>
2016-02-18 18:22:26 -0300

    Create prueba.php

---
+++ prueba.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+	<?php echo "asdasdd sad </br>"; 
+	      echo "lalalaal";
+		  echo "Primera linea php a separar de la segunda"."\n"; 
+		  echo "Esto abarca\nmúltiples líneas. Los saltos de línea también\r \nse mosctrarán.";
+	
+	?>
+</body>
+</html>
